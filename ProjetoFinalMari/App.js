2fdcebde48ec79407ce55f5ec49630d4f961aa10
Vitor <vitor.280724@alunosatc.edu.br>
2026-05-19 21:31:50 -0300

    Add files via upload

--- ProjetoFinalMari/App.js
+++ ProjetoFinalMari/App.js
@@ -1,27 +1,20 @@
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
- 
-import Login from './screens/Login';
-import Home from './screens/Home';
- 
+
+import Login        from './screens/Login';
+import Home         from './screens/Home';
+import Compromissos from './screens/Compromissos';
+
 const Stack = createNativeStackNavigator();
- 
+
 export default function App() {
   return (
     <NavigationContainer>
       <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen
-          name="Login"
-          component={Login}
-          options={{ headerShown: false }}
-        />
-        <Stack.Screen
-          name="home"
-          component={Home}
-          options={{ headerShown: false }}
-        />
+        <Stack.Screen name="Login"        component={Login}        options={{ headerShown: false }} />
+        <Stack.Screen name="home"         component={Home}         options={{ headerShown: false }} />
+        <Stack.Screen name="Compromissos" component={Compromissos} options={{ headerShown: false }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
 }
- 
