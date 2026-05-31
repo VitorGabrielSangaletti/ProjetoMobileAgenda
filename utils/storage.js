06bd180fefe37069bb5cb2722a400622c160bd3a
Batata07 <206111339+VitorGabrielSangaletti@users.noreply.github.com>
2026-05-31 13:32:43 -0300

    Formatação de alguns codigos

--- utils/storage.js
+++ utils/storage.js
@@ -3,15 +3,16 @@ import { db, auth } from './firebase'
 
 // Pega o documento do usuário logado no Firestore
 function docUsuario() {
-  const uid = auth.currentUser?.uid
-  if (!uid) throw new Error('Usuário não autenticado')
-  return doc(db, 'usuarios', uid)
+  const uid = auth.currentUser?.uid // se nao tiver usuario no firebase o uid vai ser indefinido e vai gera um erro. 
+  if (!uid) throw new Error('Usuário não autenticado') 
+  return doc(db, 'usuarios', uid) 
 }
 
+// Carrega os eventos do usuario.
 export async function loadEvents() {
   try {
     const snap = await getDoc(docUsuario())
-    if (!snap.exists()) return {}
+    if (!snap.exists()) return {} // Se o documento nao existir, retorna um objeto vazio
     return snap.data().eventos || {}
   } catch (e) {
     console.error('Erro ao carregar eventos:', e)
@@ -23,9 +24,9 @@ async function salvarTodos(eventos) {
   await setDoc(docUsuario(), { eventos })
 }
 
-export async function addEvent(data, evento) {
+export async function addEvent(data, evento) { 
   const eventos = await loadEvents()
-  if (!eventos[data]) eventos[data] = []
+  if (!eventos[data]) eventos[data] = [] // Se na data nao tiver nenhum evento cria um array vazio pra adicionar o evento
   eventos[data].push(evento)
   await salvarTodos(eventos)
   return eventos
@@ -35,7 +36,7 @@ export async function updateEvent(data, idEvento, eventoAtualizado) {
   const eventos = await loadEvents()
   if (!eventos[data]) return eventos
   eventos[data] = eventos[data].map(e =>
-    e.id === idEvento ? { ...e, ...eventoAtualizado } : e
+    e.id === idEvento ? { ...e, ...eventoAtualizado } : e 
   )
   await salvarTodos(eventos)
   return eventos
@@ -45,7 +46,7 @@ export async function deleteEvent(data, idEvento) {
   const eventos = await loadEvents()
   if (!eventos[data]) return eventos
   eventos[data] = eventos[data].filter(e => e.id !== idEvento)
-  if (eventos[data].length === 0) delete eventos[data]
+  if (eventos[data].length === 0) delete eventos[data] 
   await salvarTodos(eventos)
   return eventos
 }
